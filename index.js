17c440f2eb173759dcd43b25720755f1e2db486a
ANKIT PRATAP SINGH <jadounankit17@gmail.com>
2024-03-15 13:49:52 +0530

    change

--- index.js
+++ index.js
@@ -4,7 +4,31 @@
 // helo();
 
 
-const add= require("./app")
+// const add= require("./app")
 
 
-console.log("hello")
+// console.log("hello")
+
+
+// const os =require("os")
+// const a= os.freemem();
+// console.log(a)
+
+
+// const path=require('path');
+// console.log(__filename)
+// console.log(__dirname);
+//     console.log(path.basename(__filename));
+
+//     console.log(path.join('/hello','/hello','file.txt'))
+
+
+// const fs=require('fs')
+// fs.readFile('file.txt','utf-8',(err,data)=>{
+//     if(err){
+//         console.log(err);
+//         return
+
+//     }
+//     console.log(data)
+// })
